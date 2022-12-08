2cc7df27a11322f21c887e8205bda74e746f3881
Korigin99 <rldnjs3413@gmail.com>
2022-12-08 14:49:02 +0900

    feat : 12월 8일 python 수업

---
+++ Python/오전수업/1208/test2.py
@@ -0,0 +1,38 @@
+import random
+
+""" num = random.randint(1,3)
+
+print(num)
+
+firstname = ["김", "장", "이", "최", "윤", "변"]
+middle = ["기", "지", "영", "윤", "재", "종"]
+last = ["원","연","현","연","도"]
+
+a = random.choice(firstname)
+b = random.choice(middle)
+c = random.choice(last)
+print(a+b+c) """
+
+info = [ 
+  ["김기원", "콘샐러드", "시험"], ["이지현", "나이", "민감"], ["김지연", "배꼽", "선생님배꼽찔러봄"], ["장영주", "후크선장", "공통점"],
+  ["윤재영","비니","강균성"], ["리정수", "군대", "인민군"]
+]
+
+""" for i in info:
+  for k in i:
+    print(k) """
+
+q = input("이름 또는 특징 입력 : ")
+
+for i in info:
+  for k in i:
+    if q in k:
+      print(k)
+
+info = [ 
+  ["김기원", "콘샐러드", 13], ["이지현", "나이", 33], ["김지연", "배꼽", 34], ["장영주", "후크선장", 13],
+  ["윤재영","비니",33], ["리정수", "군대", 32]
+]
+for i in info:
+  if i[2]>=30 and i[2]<=39:
+    print(i)
